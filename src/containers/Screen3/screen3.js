b3532086c57f86746f0377f0b43465ba33e5684f
Pavel Zabalotny <pavelzabalotny@gmail.com>
2018-11-27 13:45:27 +0300

    Рефакторинг

--- src/containers/Screen3/screen3.js
+++ src/containers/Screen3/screen3.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import {StyleSheet, Text, View, WebView, Button} from 'react-native'
+import {StyleSheet, View, WebView} from 'react-native'
 import HeaderNavigationBar from "../HeaderNavigatorBar"
 
 export default class Screen3 extends React.Component {
-  /*state = {
-    isPressButton: false,
-
-  }*/
 
   render() {
-    //const {isPressButton} = this.state
 
     return (<View style={{
       flex: 1,
@@ -18,48 +13,13 @@ export default class Screen3 extends React.Component {
       <HeaderNavigationBar {...this.props} />
       <View style={{
         flex: 1,
-        /*backgroundColor: '#edb8a4',
-        alignItems: 'center',
-        justifyContent: 'center'*/
       }}>
         <WebView
           source={{uri: 'http://m.tut.by'}}
         />
-
-        {/*{
-          isPressButton
-            ? <View>
-              <Button
-                title="Go back"
-                onPress={() => {
-                  this.setState({
-                    isPressButton: false,
-                  })
-                  this.props.navigation.navigate('Screen3')
-                }}
-              />
-              <WebView
-                source={{uri: 'http://m.tut.by'}}
-              />
-            </View>
-
-            : <Button
-              onPress={this.handlePressButton}
-              title="Загрузить m.tut.by"
-              color="#841584"
-              accessibilityLabel="Learn more about this purple button"
-            />
-        }*/}
-
       </View>
     </View>)
   }
-
-  /*handlePressButton = () => {
-    this.setState({
-      isPressButton: true,
-    })
-  }*/
 }
 
 /*
